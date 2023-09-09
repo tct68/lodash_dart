9f1f55973674d604923fa09bf3d16e2f08587fe8
ThuongTC <truongquangtruong68@gmail.com>
2023-09-09 12:53:04 +0700

    Add unit test

--- example/lodash_dart_example.dart
+++ example/lodash_dart_example.dart
@@ -1,1 +1,22 @@
-void main() {}
+import 'package:lodash_dart/src/extensions/object.dart';
+
+import 'network.dart';
+
+void main() {
+  final Network network = Network(
+      name: "name",
+      chainId: 1,
+      explorers: {},
+      coinGeckoAssetPlatform: "coinGeckoAssetPlatform",
+      nativeCurrency: "nativeCurrency",
+      nativeCurrencyAddress: "nativeCurrencyAddress",
+      candideBalances: "candideBalances",
+      proxyFactory: "proxyFactory",
+      safeSingleton: "safeSingleton",
+      fallbackHandler: "fallbackHandler",
+      socialRecoveryModule: "socialRecoveryModule",
+      entrypoint: "entrypoint",
+      logo: "ssssss",
+      multiSendCall: "multiSendCall");
+  print(network.toJson());
+}
---
+++ example/network.dart
@@ -0,0 +1,40 @@
+class Network {
+  String name;
+  String? logo;
+  String? extendedLogo;
+  int chainId;
+  Map<String, dynamic> explorers;
+  String coinGeckoAssetPlatform;
+  String nativeCurrency;
+  String nativeCurrencyAddress;
+  String candideBalances;
+  String proxyFactory;
+  String safeSingleton;
+  String fallbackHandler;
+  String socialRecoveryModule;
+  String entrypoint;
+  String multiSendCall;
+  String? ensRegistryWithFallback;
+  bool visible;
+
+  String get normalizedName => name.replaceAll("ö", "oe");
+
+  Network(
+      {required this.name,
+      this.logo,
+      this.extendedLogo,
+      required this.chainId,
+      required this.explorers,
+      required this.coinGeckoAssetPlatform,
+      required this.nativeCurrency,
+      required this.nativeCurrencyAddress,
+      required this.candideBalances,
+      required this.proxyFactory,
+      required this.safeSingleton,
+      required this.fallbackHandler,
+      required this.socialRecoveryModule,
+      required this.entrypoint,
+      required this.multiSendCall,
+      this.ensRegistryWithFallback,
+      this.visible = true});
+}
